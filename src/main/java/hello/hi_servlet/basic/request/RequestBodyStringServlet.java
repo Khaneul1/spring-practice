7f7344652e1387b9a300df90572f936434a26a1e
Khaneul1 <kosky1219@gmail.com>
2025-07-09 22:56:14 +0900

    API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/hi_servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,29 @@
+package hello.hi_servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name= "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
+
+//그러나!! 보통은 이렇게 단순 텍스트로 주고받지 않음
+//자주 쓰이는 JSON으로 어떻게 주고받는지 예제 확인해 보쟈 ~~~
